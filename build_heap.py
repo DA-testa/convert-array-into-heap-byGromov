27129736c8d2e4ed9a9b9f996acbe7803a654d61
Aleksandrs Pučenkins <90133296+byGromov@users.noreply.github.com>
2023-04-02 21:22:42 +0000

    first try

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,46 @@
 # python3
+import heapq
 
+# Author: Aleksandrs Pučenkins 17.gr. 221RDB335
 
 def build_heap(data):
+
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    heapq.heapify(data)
+    for i in range(len(data)):
+        while i != 0 and data[(i - 1) // 2] > data[i]:
+            j = (i - 1) // 2
+            swaps.append((i, j))
+            data[i], data[j] = data[j], data[i]
+            i = j
     return swaps
 
 
+def read_input():
+
+    input_type = input().strip()
+    if input_type == 'I':
+        n = int(input().strip())
+        data = list(map(int, input().strip().split()))
+    elif input_type == 'F':
+        file_name = input().strip()
+        with open(file_name, 'r') as f:
+            n = int(f.readline().strip())
+            data = list(map(int, f.readline().strip().split()))
+    else:
+        raise ValueError(f"Invalid input type: {input_type}")
+    if len(data) != n:
+        raise ValueError(f"Expected {n} integers, but got {len(data)}")
+    return input_type, data
+
+
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
+    input_type, data = read_input()
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
